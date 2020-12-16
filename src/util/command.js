25340add93430b2c52016283e75c41a8c257cb93
Astrid <windows@valvesoftware.ca>
2020-12-15 16:53:59 -0800

    Created name and descrition fucntions in class commandbuilder

--- src/util/command.js
+++ src/util/command.js
@@ -9,7 +9,44 @@ module.exports = class CommandBuilder {
     this.command = data.comamnd = {
       name: null,
       description: null,
-      options: null,
+      options: [],
     };
+  }
+
+  /**
+   * Sets the name of the command
+   * @param {string} name
+   */
+  name(name) {
+    if (!name) {
+      throw new Error(
+        "[Command Error] Command names must not be empty! Please provide a proper command name."
+      );
+    }
+
+    if (name.length > 32) {
+      throw new Error(
+        "[Command Error] Command names must not be longer than 32 chars! Please provide a proper command name!"
+      );
+    }
+    this.command.name = name;
+    return this;
+  }
+
+  description(description) {
+    if (!description) {
+      throw new Error(
+        "[Command Error] Command descriptions must not be empty! Please provide a proper command name."
+      );
+    }
+
+    if (description.length > 100) {
+      throw new Error(
+        "[Command Error] Command descriptions must not be longer than 100 chars! Please provide a proper command name!"
+      );
+    }
+
+    this.command.description = description;
+    return this;
   }
 };
