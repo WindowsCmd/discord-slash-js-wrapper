d67a60366ce59773ca1aa9a64dbec6fbbee86c56
Astrid <windows@valvesoftware.ca>
2020-12-15 16:41:03 -0800

    Added client class along with basic constructor

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 const axios = require("axios");
+
+class client {
+  /**
+   * Discord bot token
+   * @param {string} token
+   *
+   * Your app id
+   * @param {strig} appId
+   */
+  constructor(token, appId) {
+    this.token = token;
+    this.appId = appId;
+  }
+}
