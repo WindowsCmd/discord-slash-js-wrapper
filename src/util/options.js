bd091b50d872c5693f624707e1e44fea24e18e27
Astrid <windows@valvesoftware.ca>
2020-12-15 17:03:30 -0800

    Built the options builder

--- src/util/options.js
+++ src/util/options.js
@@ -1,3 +1,91 @@
 "use-strict";
 
-module.exports = class OptionsBuilder {};
+module.exports = class OptionsBuilder {
+  constructor() {
+    this.setup({});
+  }
+
+  setup(data) {
+    this.option = data.options = {
+      name: null,
+      description: null,
+      type: null,
+      required: null,
+      choices: [],
+    };
+  }
+
+  /**
+   * The name of the option
+   * @param {string} name
+   */
+  name(name) {
+    if (!name) {
+      throw new Error(
+        "[Options Error] Option names must not be empty! Please provide a proper Option name."
+      );
+    }
+
+    if (name.length > 32) {
+      throw new Error(
+        "[Options Error] Option names must not be longer than 32 chars! Please provide a proper Option name!"
+      );
+    }
+    this.option.name = name;
+    return this;
+  }
+
+  /**
+   * The description of the option
+   * @param {string} description
+   */
+  description(description) {
+    if (!description) {
+      throw new Error(
+        "[Options Error] Option descriptions must not be empty! Please provide a proper Option description."
+      );
+    }
+
+    if (description.length > 100) {
+      throw new Error(
+        "[Options Error] Option descriptions must not be longer than 100 chars! Please provide a proper Option description!"
+      );
+    }
+
+    this.option.description = description;
+    return this;
+  }
+
+  /**
+   * If the command is required
+   * @param {bool} required
+   * @default null
+   */
+  required(required) {
+    if (required == null) {
+      throw new Error("[Options Error] Required must not be null!");
+    }
+    this.option.required = required;
+    return this;
+  }
+
+  newChoice(name, value) {
+    if (!name) {
+      throw new Error("[Options Error] Choice name must not be null!");
+    }
+    if (!value) {
+      throw new Error("[Options Error] Choice value must not be null!");
+    }
+
+    this.option.choices.push({ name: name, value: value });
+    return this;
+  }
+
+  type(type) {
+    if (!type) {
+      throw new Error("[Options Error] Type must not be null");
+    }
+    this.option.type = type;
+    return this;
+  }
+};
